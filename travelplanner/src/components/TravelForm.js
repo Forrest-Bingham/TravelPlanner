3f058577c901607d0d5e8c0c0c7786d08d20a1da
Forrest Bingham <bingham_5@live.com>
2020-11-11 20:28:50 -0700

    Added form and itinerary

--- travelplanner/src/components/TravelForm.js
+++ travelplanner/src/components/TravelForm.js
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
+import Trip from "./Trip";
+import "./Body.css";
+
 
 const TravelForm = () => {
+
+    var trip = {start:"", end:""};
 
     const [from, setFrom] = useState("");
     const [to, setTo] = useState("");
@@ -9,26 +14,49 @@ const TravelForm = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
+        trip.start = from
+        trip.end = to
+        console.log("Trip ", trip)
+        itinerary.push(trip)
+        console.log(itinerary, "Itinerary")
+
+        setFrom(to)
+        setTo("")
+        setPlan("")
+
     }
 
     return(
-        <div>
+        <div className="planner">
         <form onSubmit={handleSubmit} className="form">
             <input
                 type="text"
                 name="from"
                 value={from}
-                placeholder="From"
+                placeholder=" From"
                 onChange={e => setFrom(e.target.value)}
+                className="from"
             />
             <input
                 type="text"
                 name="to"
                 value={to}
-                placeholder="To"
+                placeholder=" To"
                 onChange={e => setTo(e.target.value)}
+                className="to"
             />
+            <button type="submit" className="formButton">Book It</button>
         </form>
+        <div className="itinerary">
+            <h2>Itinerary</h2>
+            {itinerary.map(flight => (
+                <Trip
+                key={flight.start}
+                start={flight.start}
+                end={flight.end}
+                />
+            ))}
+        </div>
         </div>
     )
 }
